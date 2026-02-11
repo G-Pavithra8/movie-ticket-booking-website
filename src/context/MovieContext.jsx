1e2536504c81055ef5226d387057521269f2e081
pavithra <pavithraganapathy04@gmail.com>
2026-02-11 22:00:36 +0530

    Fix ESLint dependency error

--- src/context/MovieContext.jsx
+++ src/context/MovieContext.jsx
@@ -1,4 +1,5 @@
-import React, { createContext, useState, useContext, useEffect } from 'react';
+import React, { createContext, useState, useContext, useEffect, useCallback } from 'react';
+
 import { fetchMovies, searchMovies } from '../services/movieApi';
 
 const MovieContext = createContext();
